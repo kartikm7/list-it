274521eaf7f9f278917ba66f461ec45c6293e8c2
kartikm7 <kartikeyavkm@gmail.com>
2024-10-30 13:54:34 +0000

    feat: add new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,22 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :desc, :task_type)
+  end
 end
